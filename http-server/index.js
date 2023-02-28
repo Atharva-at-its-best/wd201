c14041e0fc7932d16e6e4ac99bbbf3ff8f94819b
Atharva-at-its-best <125311829+Atharva-at-its-best@users.noreply.github.com>
2023-02-28 21:57:58 +0530

    Added new index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const readline = require("readline");
 
 let homeContent = "";
 let projectContent = "";
@@ -27,57 +28,72 @@ fs.readFile("registration.html", (err, registration) => {
 });
 
 const portIndex = process.argv.indexOf('--port');
-const port = process.argv[portIndex + 1] || 5000;
+let port = 5000;
+if (portIndex !== -1 && process.argv[portIndex + 1]) {
+    port = parseInt(process.argv[portIndex + 1], 10);
+} else {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question("Enter port number: ", (answer) => {
+        port = parseInt(answer, 10);
+        rl.close();
+        startServer();
+    });
+}
 
-http.createServer((request, response) => {
-    let url = request.url;
-    if (url === '/webpage.css') {
-        response.writeHead(200, { 'Content-Type': 'text/css' });
-        fs.readFile('webpage.css', 'utf8', (err, css) => {
-            if (err) {
-                response.writeHead(404);
-                response.end();
-            } else {
-                response.end(css);
+function startServer() {
+    http.createServer((request, response) => {
+        let url = request.url;
+        if (url === '/webpage.css') {
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+            fs.readFile('webpage.css', 'utf8', (err, css) => {
+                if (err) {
+                    response.writeHead(404);
+                    response.end();
+                } else {
+                    response.end(css);
+                }
+            });
+        } else {
+            response.writeHead(200);
+            switch (url) {
+                case '/project':
+                    response.write(projectContent);
+                    break;
+                case '/registration':
+                    response.write(registrationContent);
+                    break;
+                case '/webpage.js':
+                    response.writeHead(200, { 'Content-Type': 'text/javascript' });
+                    fs.readFile('webpage.js', 'utf8', (err, js) => {
+                        if (err) {
+                            response.writeHead(404);
+                            response.end();
+                        } else {
+                            response.end(js);
+                        }
+                    });
+                    return;
+                case '/webpage.jpg':
+                    response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+                    fs.readFile('webpage.jpg', (err, image) => {
+                        if (err) {
+                            response.writeHead(404);
+                            response.end();
+                        } else {
+                            response.end(image);
+                        }
+                    });
+                    return;
+                default:
+                    response.write(homeContent);
+                    break;
             }
-        });
-    } else {
-        response.writeHead(200);
-        switch (url) {
-            case '/project':
-                response.write(projectContent);
-                break;
-            case '/registration':
-                response.write(registrationContent);
-                break;
-            case '/webpage.js':
-                response.writeHead(200, { 'Content-Type': 'text/javascript' });
-                fs.readFile('webpage.js', 'utf8', (err, js) => {
-                    if (err) {
-                        response.writeHead(404);
-                        response.end();
-                    } else {
-                        response.end(js);
-                    }
-                });
-                return;
-            case '/webpage.jpg':
-                response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-                fs.readFile('webpage.jpg', (err, image) => {
-                    if (err) {
-                        response.writeHead(404);
-                        response.end();
-                    } else {
-                        response.end(image);
-                    }
-                });
-                return;
-            default:
-                response.write(homeContent);
-                break;
+            response.end();
         }
-        response.end();
-    }
-}).listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
+    }).listen(port, () => {
+        console.log(`Server running at http://localhost:${port}/`);
+    });
+}
