1fea852b90996ca0f2252174699c1b3cde2146df
Atharva-at-its-best <125311829+Atharva-at-its-best@users.noreply.github.com>
2023-03-15 23:40:21 +0530

    Added this new Files

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,29 +1,30 @@
 const todoList = () => {
-    all = []
+    const today = new Date().toLocaleDateString("en-CA");
+    const all = [];
     const add = (todoItem) => {
-        all.push(todoItem)
-    }
+        all.push(todoItem);
+    };
     const markAsComplete = (index) => {
-        all[index].completed = true
-    }
+        all[index].completed = true;
+    };
 
     const overdue = () => {
         return all.filter((item) => {
-            return item.dueDate < today && !item.completed
-        })
-    }
+            return item.dueDate < today && !item.completed;
+        });
+    };
 
     const dueToday = () => {
         return all.filter((item) => {
-            return item.dueDate === today && !item.completed || item.completed
-        })
-    }
+            return (item.dueDate === today && !item.completed) || item.completed;
+        });
+    };
 
     const dueLater = () => {
         return all.filter((item) => {
-            return item.dueDate > today && !item.completed
-        })
-    }
+            return item.dueDate > today && !item.completed;
+        });
+    };
 
     const toDisplayableList = (list) => {
         return list
@@ -32,7 +33,7 @@ const todoList = () => {
                 return `[${item.completed ? "x" : " "}] ${item.title} ${formateddateoutput}`;
             })
             .join("\n");
-    }
+    };
 
     return {
         all,
@@ -45,47 +46,4 @@ const todoList = () => {
     };
 };
 
-// ####################################### #
-// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
-// ####################################### #
-
-const todos = todoList();
-
-const formattedDate = (d) => {
-    return d.toISOString().split("T")[0];
-};
-
-var dateToday = new Date();
-const today = formattedDate(dateToday);
-const yesterday = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() - 1))
-);
-const tomorrow = formattedDate(
-    new Date(new Date().setDate(dateToday.getDate() + 1))
-);
-
-todos.add({ title: "Submit assignment", dueDate: yesterday, completed: false });
-todos.add({ title: "Pay rent", dueDate: today, completed: true });
-todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
-todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
-todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
-
-console.log("My Todo-list\n");
-
-console.log("Overdue");
-var overdues = todos.overdue();
-var formattedOverdues = todos.toDisplayableList(overdues);
-console.log(formattedOverdues);
-console.log("\n");
-
-console.log("Due Today");
-let itemsDueToday = todos.dueToday();
-let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday);
-console.log(formattedItemsDueToday);
-console.log("\n");
-
-console.log("Due Later");
-let itemsDueLater = todos.dueLater();
-let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater);
-console.log(formattedItemsDueLater);
-console.log("\n\n");
+module.exports = todoList;
